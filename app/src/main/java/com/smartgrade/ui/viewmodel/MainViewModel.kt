13b390914b85272538bbd97b7eb6e5e41821be15
Victor Bastos <vbastosmaciel@outlook.com.br>
2024-05-08 22:25:15 -0300

    fix: Fixing unused imports

--- app/src/main/java/com/smartgrade/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/smartgrade/ui/viewmodel/MainViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.smartgrade.data.local.model.Subject
 import com.smartgrade.data.local.model.relationship.SubjectPoints
 import com.smartgrade.data.repository.SubjectRepository
 import kotlinx.coroutines.launch
